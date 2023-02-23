c739cba715111d33e43bc7d190c5627bb12cea4d
Igor Boiko <igorboiko@gmail.com>
2023-02-23 22:44:48 +0200

    add 03task solution

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,15 +8,43 @@ const refs = {
 };
 // Ключ для зберігання в Local Storage
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+// Викликаємо функцію для заповнення значень які ще не були відправлені і зберігаються в локальному сховищі
+populateInputs();
+// Додавання прослуховувача на кожну зміну елементів форми кожні 500мс
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
 // Callback-функція для прослуховувача (при зміні значення елементів інпута)
 function onInput(e) {
-  // Створення об'єкту для зберігання даних в Local Storage
+  // Створення об'єкту для зберігання невідправлених даних в Local Storage
   const objectToLocal = {
     email: refs.inputEmail.value,
     message: refs.textareaMessage.value,
   };
-  // Запис даних обєктом в Local Storage
+  // Запис невідправлених даних обєктом в Local Storage
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToLocal));
 }
-// Додавання прослуховувача на кожну зміну елементів форми кожні 500мс
-refs.form.addEventListener('input', throttle(onInput, 500));
+// Callback-функція для прослуховувача на кнопці (при відправці форми)
+function onFormSubmit(e) {
+  // відміняємо дію за замовчуванням
+  e.preventDefault();
+  // видаляємо значення з локального сховища
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  // Очищаємо поля форми
+  e.currentTarget.reset();
+}
+// Функція для заповнення значень інпутів, які ще не були відправлені і зберігаються в локальному сховищі
+function populateInputs() {
+  // Використовуємо конструкцію try...catch щоб скрипт не падав
+  try {
+    // дістаємо значення з локального сховища
+    const savedState = localStorage.getItem(LOCALSTORAGE_KEY);
+    // Якщо локальне сховище зберігало значення, то заповненюємо ними інпути форми
+    if (savedState) {
+      refs.inputEmail.value = JSON.parse(savedState).email;
+      refs.textareaMessage.value = JSON.parse(savedState).message;
+    }
+  } catch (err) {
+    console.error('Проблеми з доступом до локальних даних:', err);
+  }
+}
